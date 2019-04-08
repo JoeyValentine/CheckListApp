e1182b115362786fccfe2c03a5fb6630d1ab4292
JoeyValentine <rlaalsdn0506@naver.com>
2019-04-08 15:41:06 +0900

    Initial Commit

---
+++ myCheckListTests/myCheckListTests.swift
@@ -0,0 +1,34 @@
+//
+//  myCheckListTests.swift
+//  myCheckListTests
+//
+//  Created by 김민우 on 08/04/2019.
+//  Copyright © 2019 김민우. All rights reserved.
+//
+
+import XCTest
+@testable import myCheckList
+
+class myCheckListTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
